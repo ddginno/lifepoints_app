17d349ff64e45f4bf246d7c2b34d6d13c8e86091
David Dias Graxinha <david@blvd-nights.de>
2019-07-26 14:53:17 +0200

    add card final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Card from "./components/Card";
 import Container from "./components/Container";
+import PropTypes from "prop-types";
 
 function App() {
   return (
     <Container className="App">
-      <Card />
+      <Card
+        titleContent="LIMITED EDITION"
+        subtitleContent="NIKES NEUER SUPER SCHUH DER SUPERLATIVE"
+      />
     </Container>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,55 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import ContenImageCard from "./ContentImageCard";
 
 const StyledCard = styled.div`
-  padding: 2px;
   background: #6d7278;
-  width: 100vw;
-  border: 2px solid #979797;
+  width: 100%;
+
   border-radius: 1px;
   position: relative;
 `;
 
 const StyledTitle = styled.p`
+  color: #0ae5f5;
+  margin-bottom: 0px;
+  margin-top: 1px;
+`;
+const StyledSubtitle = styled.p`
   color: white;
+  margin-top: 1px;
 `;
 
-function Card() {
+const ContentImageCard = styled.img`
+  box-sizing: border-box;
+  height: ${props => props.height || "auto"};
+  width: ${props => props.width || "100%"};
+  border: 2px solid #979797;
+`;
+
+const ContentContainer = styled.div`
+  padding-left: 10px;
+  font-family: sans-serif;
+  border: 2px solid #979797;
+  margin: 0px;
+`;
+
+function Card({ titleContent, subtitleContent }) {
   return (
-    <StyledCard>
-      <ContenImageCard />
-      <StyledTitle>NIKES NEUER SUPER SCHUH DER SUPERLATIVE</StyledTitle>
+    <StyledCard onClick>
+      <ContentImageCard alt="" src="Nike.jpg" />
+      <ContentContainer>
+        <StyledTitle>{titleContent}</StyledTitle>
+        <StyledSubtitle>{subtitleContent}</StyledSubtitle>
+      </ContentContainer>
     </StyledCard>
   );
 }
 
 Card.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired
+  titleContent: PropTypes.string.isRequired,
+  subtitleContent: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired
 };
 export default Card;
